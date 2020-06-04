82b828d51d78fe36849edb6609bac43237ce4c03
Adam <aalston9@gmail.com>
2020-06-03 23:39:09 -0400

    data encoding

---
+++ getdata_encoding.php
@@ -0,0 +1,9 @@
+<?php
+   $conn = new mysqli("localhost", "root", "seedubuntu", "dbtest");
+   $pid = $mysqli->real_escape_string($_GET['PID']);         
+   $pwd = $mysqli->real_escape_string($_GET['Password']);    
+   $sql = "SELECT Name, Salary, SSN
+           FROM employee
+           WHERE pid= '$pid' and password='$pwd'";
+?>
+
