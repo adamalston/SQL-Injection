93e84a9cfbf8c0b84f21932d6a71ba7ef940cdb1
Adam <aalston9@gmail.com>
2020-06-05 11:35:45 -0400

    encode with escape strings

--- getdata-encoding.php
+++ getdata-encoding.php
@@ -1,7 +1,7 @@
 <?php
    $conn = new mysqli("localhost", "root", "seedubuntu", "dbtest");
-   $pid = $mysqli->real_escape_string($_GET['PID']);         
-   $pwd = $mysqli->real_escape_string($_GET['Password']);    
+   $pid = $mysqli->real_escape_string($_GET['PID']);
+   $pwd = $mysqli->real_escape_string($_GET['Password']);
    $sql = "SELECT Name, Salary, SSN
            FROM employee
            WHERE pid= '$pid' and password='$pwd'";
