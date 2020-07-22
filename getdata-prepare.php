2c4fab68a79abb4ab3b12f1c17aca4bca414b108
Adam <aalston9@gmail.com>
2020-07-22 00:23:15 -0400

    db.execute()

--- getdata-prepare.php
+++ getdata-prepare.php
@@ -1,15 +1,15 @@
 <?php
-   $conn = new mysqli("localhost", "root", "seedubuntu", "dbtest");
-   $sql = "SELECT Name, Salary, SSN
-           FROM employee
-           WHERE pid= ? and password=?";
-   if ($stmt = $conn->prepare($sql)) {
-       $stmt->bind_param("ss", $pid, $pwd);
-       $stmt->execute();
-       $stmt->bind_result($name, $salary, $ssn);
-       while ($stmt->fetch()) {
-           printf("%s %s %s\n", $name, $salary, $ssn);
-       }
-   }
+    $conn = new mysqli("localhost", "root", "seedubuntu", "dbtest");
+    $sql = "SELECT Name, Salary, SSN
+            FROM employee
+            WHERE pid= ? and password=?";
+    if ($stmt = $conn->prepare($sql)) {
+        $stmt->bind_param("ss", $pid, $pwd);
+        $stmt->execute();
+        $stmt->bind_result($name, $salary, $ssn);
+        while ($stmt->fetch()) {
+            printf("%s %s %s\n", $name, $salary, $ssn);
+        }
+    }
 ?>
 
