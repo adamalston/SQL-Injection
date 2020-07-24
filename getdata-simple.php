d7021c40445d9da828d0e158e040d9e678866f99
Adam <aalston9@gmail.com>
2020-07-24 00:36:59 -0400

    get ID and password

--- getdata-simple.php
+++ getdata-simple.php
@@ -1,5 +1,5 @@
 <?php
-   $pid = $_GET['PID'];
-   $pwd = $_GET['Password'];
-   echo "PID: $pid --- Password: $pwd\n";
+    $pid = $_GET['PID'];
+    $pwd = $_GET['Password'];
+    echo "PID: $pid --- Password: $pwd\n";
 ?>
