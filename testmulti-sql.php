986248095c3b43624c974b841cfc5514329ef653
Adam <aalston9@gmail.com>
2020-07-26 00:25:41 -0400

    create test db -> drop test db

--- testmulti-sql.php
+++ testmulti-sql.php
@@ -1,7 +1,8 @@
 <?php
-  $mysqli = new mysqli("localhost", "root", "seedubuntu", "dbtest");
-  $res    = $mysqli->query("SELECT 1; DROP DATABASE dbtest");
-  if (!$res) {
-    echo "Error executing query: (" . $mysqli->errno . ") " . $mysqli->error;
-  }
+    $mysqli = new mysqli("localhost", "root", "seedubuntu", "dbtest");
+    $res    = $mysqli->query("SELECT 1; DROP DATABASE dbtest");
+
+    if (!$res) {
+        echo "Error executing query: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
 ?>
